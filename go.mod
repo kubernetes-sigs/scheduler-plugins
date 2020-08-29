4923de738f164445033aad62d13339a97887e234
Wei Huang <wei.huang1@ibm.com>
2020-08-28 23:07:24 -0700

    Fix openapi-gen script

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	k8s.io/client-go v0.19.0
 	k8s.io/code-generator v0.19.0
 	k8s.io/klog/v2 v2.2.0
+	k8s.io/kube-openapi v0.0.0-20200805222855-6aeccd4b50c6
 	k8s.io/kubernetes v1.19.0
 )
 
--- hack/tools.go
+++ hack/tools.go
@@ -18,4 +18,7 @@ limitations under the License.
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
-import _ "k8s.io/code-generator"
+import (
+	_ "k8s.io/code-generator"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+)
