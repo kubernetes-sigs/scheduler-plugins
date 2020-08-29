8d2cbfd04b86d038e200f25740e5a326ee262ae5
Wei Huang <wei.huang1@ibm.com>
2020-08-28 21:26:04 -0700

    Core logic adapting to v1.19.0
    - Reuse in-tree scheme to register out-of-tree Args
    - A trick to enforce initilization of schem
    - Adjust method signature and UT helpers

--- pkg/apis/config/scheme/scheme.go
+++ pkg/apis/config/scheme/scheme.go
@@ -20,14 +20,15 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	kubeschedulerscheme "k8s.io/kubernetes/pkg/scheduler/apis/config/scheme"
 
 	"sigs.k8s.io/scheduler-plugins/pkg/apis/config"
 	configv1beta1 "sigs.k8s.io/scheduler-plugins/pkg/apis/config/v1beta1"
 )
 
 var (
-	// Scheme is the runtime.Scheme to which all kubescheduler api types are registered.
-	Scheme = runtime.NewScheme()
+	// Re-use the in-tree Scheme.
+	Scheme = kubeschedulerscheme.Scheme
 
 	// Codecs provides access to encoding and decoding for the scheme.
 	Codecs = serializer.NewCodecFactory(Scheme, serializer.EnableStrict)
