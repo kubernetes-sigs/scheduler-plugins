fb7b1a814c27db7b7c3d7c7e35d431f9c0bd3b31
Wei Huang <wei.huang1@ibm.com>
2021-10-28 16:06:59 -0700

    go module files
    - bump k8s versions
    - bump load-watcher version

--- pkg/generated/clientset/versioned/fake/clientset_generated.go
+++ pkg/generated/clientset/versioned/fake/clientset_generated.go
@@ -74,7 +74,10 @@ func (c *Clientset) Tracker() testing.ObjectTracker {
 	return c.tracker
 }
 
-var _ clientset.Interface = &Clientset{}
+var (
+	_ clientset.Interface = &Clientset{}
+	_ testing.FakeClient  = &Clientset{}
+)
 
 // SchedulingV1alpha1 retrieves the SchedulingV1alpha1Client
 func (c *Clientset) SchedulingV1alpha1() schedulingv1alpha1.SchedulingV1alpha1Interface {
