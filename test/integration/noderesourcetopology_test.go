b86cfe93d9d678d5e7f3b0b36eacbeba2173be24
Talor Itzhak <titzhak@redhat.com>
2022-05-01 14:27:32 +0300

    nrt: integration-tests: adding scope=container tests
    Add batch of tests that are validating the flow of NodeResourceTopology plugin when Kubelet configured with TopologyManagerScope=container.
    
    In case of tests that are expecting the pod to keep in Pending state, we are expecting
    an Event with a very specific error message that should be reported by the NodeResourceTopology plugin.
    This is done in order to make sure the test is not falling due to
    other plugins like NodeResourceFit plugin.
    
    Each test has a type and tier level which are correspond to the table in the TESTS.md file.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- test/integration/noderesourcetopology_test.go
+++ test/integration/noderesourcetopology_test.go
@@ -19,6 +19,7 @@ package integration
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -48,8 +49,10 @@ import (
 )
 
 const (
-	cpu    = string(v1.ResourceCPU)
-	memory = string(v1.ResourceMemory)
+	cpu             = string(v1.ResourceCPU)
+	memory          = string(v1.ResourceMemory)
+	hugepages2Mi    = "hugepages-2Mi"
+	nicResourceName = "vendor/nic1"
 )
 
 var (
@@ -57,6 +60,25 @@ var (
 	balancedAllocationScheduler = fmt.Sprintf("%v-scheduler", string(scheconfig.BalancedAllocation))
 	leastAllocatedScheduler     = fmt.Sprintf("%v-scheduler", string(scheconfig.LeastAllocated))
 )
+
+// should be filled by the user
+type nrtTestUserEntry struct {
+	// description contains the test type and tier as described in noderesourcetopology/TESTS.md
+	// and a short description of the test itself
+	description string
+	initCntReq  []map[string]string
+	cntReq      []map[string]string
+	errMsg      string
+	// this testing batch is going to be run against the same node and NRT objects, hence we're not specifying them.
+}
+
+type nrtTestEntry struct {
+	name                   string
+	pods                   []*v1.Pod
+	nodeResourceTopologies []*topologyv1alpha1.NodeResourceTopology
+	expectedNodes          []string
+	errMsg                 string
+}
 
 func TestTopologyMatchPlugin(t *testing.T) {
 	testCtx := &testContext{}
@@ -129,9 +151,11 @@ func TestTopologyMatchPlugin(t *testing.T) {
 
 	// Create a Node.
 	resList := map[v1.ResourceName]string{
-		v1.ResourceCPU:    "16",
-		v1.ResourceMemory: "100Gi",
+		v1.ResourceCPU:    "64",
+		v1.ResourceMemory: "128Gi",
 		v1.ResourcePods:   "32",
+		hugepages2Mi:      "896Mi",
+		nicResourceName:   "48",
 	}
 	for _, nodeName := range []string{"fake-node-1", "fake-node-2"} {
 		newNode := st.MakeNode().Name(nodeName).Label("node", nodeName).Capacity(resList).Obj()
@@ -578,32 +602,259 @@ func TestTopologyMatchPlugin(t *testing.T) {
 					map[string]string{cpu: "4", memory: "6Gi"}, false).Obj(),
 			},
 			nodeResourceTopologies: []*topologyv1alpha1.NodeResourceTopology{
-				makeNodeResourceTopology("fake-node-1", topologyv1alpha1.SingleNUMANodeContainerLevel,
-					[]topologyv1alpha1.ResourceInfoList{
-						{
+				MakeNRT().Name("fake-node-1").Policy(topologyv1alpha1.SingleNUMANodeContainerLevel).
+					Zone(
+						topologyv1alpha1.ResourceInfoList{
 							noderesourcetopology.MakeTopologyResInfo(cpu, "2", "2"),
 							noderesourcetopology.MakeTopologyResInfo(memory, "8Gi", "8Gi"),
-						},
-						{
+						}).
+					Zone(
+						topologyv1alpha1.ResourceInfoList{
 							noderesourcetopology.MakeTopologyResInfo(cpu, "2", "2"),
 							noderesourcetopology.MakeTopologyResInfo(memory, "8Gi", "8Gi"),
-						},
-					}),
-				makeNodeResourceTopology("fake-node-2", topologyv1alpha1.SingleNUMANodeContainerLevel,
-					[]topologyv1alpha1.ResourceInfoList{
-						{
+						}).Obj(),
+				MakeNRT().Name("fake-node-2").Policy(topologyv1alpha1.SingleNUMANodeContainerLevel).
+					Zone(
+						topologyv1alpha1.ResourceInfoList{
 							noderesourcetopology.MakeTopologyResInfo(cpu, "4", "4"),
 							noderesourcetopology.MakeTopologyResInfo(memory, "8Gi", "8Gi"),
-						},
-						{
+						}).
+					Zone(
+						topologyv1alpha1.ResourceInfoList{
 							noderesourcetopology.MakeTopologyResInfo(cpu, "4", "4"),
 							noderesourcetopology.MakeTopologyResInfo(memory, "8Gi", "3Gi"),
-						},
-					}),
+						}).Obj(),
 			},
 			expectedNodes: []string{},
 		},
-	} {
+	}
+
+	// a dedicated set of tests for the scope=container flow
+	scopeEqualsContainerTests := []nrtTestUserEntry{
+		{
+			description: "[4][tier1] multi containers with good devices and hugepages allocation, spread across NUMAs - fit",
+			cntReq: []map[string]string{
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "500Mi", nicResourceName: "16"},
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "50Mi", nicResourceName: "8"},
+			},
+		},
+		{
+			description: "[5][tier1] multi containers with hugepages over allocation, spread across NUMAs - not fit",
+			cntReq: []map[string]string{
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "400Mi"},
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "400Mi"},
+			},
+			errMsg: "cannot align container: cnt-2",
+		},
+		{
+			description: "[5][tier1] multi containers with device over allocation, spread across NUMAs - not fit",
+			cntReq: []map[string]string{
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "50Mi", nicResourceName: "20"},
+				{cpu: "2", memory: "6Gi", hugepages2Mi: "500Mi", nicResourceName: "20"},
+			},
+			errMsg: "cannot align container: cnt-2",
+		},
+		{
+			description: "[7][tier1] init container with cpu over allocation, multi-containers with good allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "40", memory: "40Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "1", memory: "4Gi"},
+				{cpu: "1", memory: "4Gi"},
+			},
+			errMsg: "cannot align init container: initcnt-1",
+		},
+		{
+			description: "[7][tier1] init container with memory over allocation, multi-containers with good allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "70Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "1", memory: "4Gi"},
+				{cpu: "1", memory: "4Gi"},
+			},
+			errMsg: "cannot align init container: initcnt-1",
+		},
+		{
+			description: "[11][tier1] init container with good allocation, multi-containers spread across NUMAs - fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+			},
+		},
+		{
+			description: "[12][tier1] init container with good allocation, multi-containers with cpu over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "10Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[12][tier1] init container with good allocation, multi-containers with memory over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "2", memory: "40Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[17][tier1] multi init containers with good allocation, multi-containers spread across NUMAs - fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "6", memory: "10Gi"},
+			},
+		},
+		{
+			description: "[18][tier1] multi init containers with good allocation, multi-containers with cpu over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "10Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[18][tier1] multi init containers with good allocation, multi-containers with memory over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+				{cpu: "4", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "35Gi"},
+				{cpu: "20", memory: "35Gi"},
+				{cpu: "2", memory: "50Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[24][tier1] multi init containers with good allocation, multi-containers with cpu over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "6Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[24][tier1] multi init containers with good allocation, multi-containers with memory over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "35Gi"},
+				{cpu: "20", memory: "35Gi"},
+				{cpu: "2", memory: "50Gi"},
+			},
+			errMsg: "cannot align container: cnt-3",
+		},
+		{
+			description: "[27][tier1] multi init containers with good allocation, container with cpu over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "35", memory: "40Gi"},
+			},
+			errMsg: "cannot align container: cnt-1",
+		},
+		{
+			description: "[28][tier1] multi init containers with good allocation, multi-containers with good allocation - fit",
+			initCntReq: []map[string]string{
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+			},
+		},
+		{
+			description: "[29][tier1] multi init containers when sum of their cpus requests (together) is over allocatable, multi-containers with good allocation - fit",
+			initCntReq: []map[string]string{
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+				{cpu: "30", memory: "10Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "2", memory: "6Gi"},
+			},
+		},
+		{
+			description: "[29][tier1] multi init containers when sum of their memory requests (together) is over allocatable, multi-containers with good allocation - fit",
+			initCntReq: []map[string]string{
+				{cpu: "3", memory: "50Gi"},
+				{cpu: "3", memory: "50Gi"},
+				{cpu: "3", memory: "50Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "2", memory: "6Gi"},
+			},
+		},
+		{
+			description: "[32][tier1] multi init containers with cpu over allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "40", memory: "50Gi"},
+				{cpu: "3", memory: "50Gi"},
+				{cpu: "3", memory: "50Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "2", memory: "6Gi"},
+			},
+			errMsg: "cannot align init container: initcnt-1",
+		},
+		{
+			description: "[32][tier1] multi init containers with over memory allocation - not fit",
+			initCntReq: []map[string]string{
+				{cpu: "20", memory: "50Gi"},
+				{cpu: "20", memory: "65Gi"},
+				{cpu: "3", memory: "50Gi"},
+			},
+			cntReq: []map[string]string{
+				{cpu: "20", memory: "40Gi"},
+				{cpu: "2", memory: "6Gi"},
+			},
+			errMsg: "cannot align init container: initcnt-2",
+		},
+	}
+	tests = append(tests, parseTestUserEntry(scopeEqualsContainerTests, ns)...)
+
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Logf("Start-topology-match-test %v", tt.name)
 			defer cleanupNodeResourceTopologies(testCtx.Ctx, extClient, tt.nodeResourceTopologies)
@@ -647,8 +898,20 @@ func TestTopologyMatchPlugin(t *testing.T) {
 					// if tt.expectedNodes == 0 we don't expect the pod to get scheduled
 				} else {
 					// wait for the pod scheduling to failed.
-					if err := wait.Poll(1*time.Second, 20*time.Second, func() (bool, error) {
-						return !podScheduled(cs, ns, p.Name), nil
+					if err := wait.Poll(5*time.Second, 20*time.Second, func() (bool, error) {
+						events, err := podFailedScheduling(cs, ns, p.Name)
+						if err != nil {
+							// This could be a connection error, so we want to retry.
+							klog.ErrorS(err, "Failed check pod scheduling status for pod", "pod", klog.KRef(ns, p.Name))
+							return false, nil
+						}
+						for _, e := range events {
+							if strings.Contains(e.Message, tt.errMsg) {
+								return true, nil
+							}
+							klog.Warningf("Pod failed but error message does not contain substring: %q; got %q instead", tt.errMsg, e.Message)
+						}
+						return false, nil
 					}); err != nil {
 						t.Errorf("pod %q scheduling should failed, error: %v", p.Name, err)
 					}
@@ -711,14 +974,14 @@ func withLimits(p *st.PodWrapper, resMap map[string]string, initContainer bool) 
 	var cntName string
 	if initContainer {
 		containers = &p.Obj().Spec.InitContainers
-		cntName = "initcon"
+		cntName = "initcnt"
 	} else {
 		containers = &p.Obj().Spec.Containers
-		cntName = "con"
+		cntName = "cnt"
 	}
 
 	*containers = append(*containers, v1.Container{
-		Name:  fmt.Sprintf("%s-%d", cntName, len(*containers)),
+		Name:  fmt.Sprintf("%s-%d", cntName, len(*containers)+1),
 		Image: imageutils.GetPauseImageName(),
 		Resources: v1.ResourceRequirements{
 			Limits: res,
@@ -771,20 +1034,115 @@ func makeResourceAllocationScoreArgs(strategy *scheconfig.ScoringStrategy) *sche
 	}
 }
 
-func makeNodeResourceTopology(name string, tmp topologyv1alpha1.TopologyManagerPolicy, resInfoLists []topologyv1alpha1.ResourceInfoList) *topologyv1alpha1.NodeResourceTopology {
-	nrt := &topologyv1alpha1.NodeResourceTopology{
-		ObjectMeta:       metav1.ObjectMeta{Name: name},
-		TopologyPolicies: []string{string(tmp)},
-		Zones:            topologyv1alpha1.ZoneList{},
+type nrtWrapper struct {
+	nrt topologyv1alpha1.NodeResourceTopology
+}
+
+func MakeNRT() *nrtWrapper {
+	return &nrtWrapper{topologyv1alpha1.NodeResourceTopology{}}
+}
+
+func (n *nrtWrapper) Name(name string) *nrtWrapper {
+	n.nrt.Name = name
+	return n
+}
+
+func (n *nrtWrapper) Policy(policy topologyv1alpha1.TopologyManagerPolicy) *nrtWrapper {
+	n.nrt.TopologyPolicies = append(n.nrt.TopologyPolicies, string(policy))
+	return n
+}
+
+func (n *nrtWrapper) Zone(resInfo topologyv1alpha1.ResourceInfoList) *nrtWrapper {
+	z := topologyv1alpha1.Zone{
+		Name:      fmt.Sprintf("node-%d", len(n.nrt.Zones)),
+		Type:      "Node",
+		Resources: resInfo,
+	}
+	n.nrt.Zones = append(n.nrt.Zones, z)
+	return n
+}
+
+func (n *nrtWrapper) Obj() *topologyv1alpha1.NodeResourceTopology {
+	return &n.nrt
+}
+
+func parseTestUserEntry(entries []nrtTestUserEntry, ns string) []nrtTestEntry {
+	var teList []nrtTestEntry
+	for i, e := range entries {
+		p := st.MakePod().Name(fmt.Sprintf("%s-%d", "topology-aware-scheduler-pod", i+1)).Namespace(ns)
+		for _, req := range e.initCntReq {
+			p = withLimits(p, req, true)
+		}
+
+		for _, req := range e.cntReq {
+			p = withLimits(p, req, false)
+		}
+		nodeTopologies := []*topologyv1alpha1.NodeResourceTopology{
+			MakeNRT().Name("fake-node-1").Policy(topologyv1alpha1.SingleNUMANodeContainerLevel).
+				Zone(
+					topologyv1alpha1.ResourceInfoList{
+						noderesourcetopology.MakeTopologyResInfo(cpu, "32", "30"),
+						noderesourcetopology.MakeTopologyResInfo(memory, "64Gi", "60Gi"),
+						noderesourcetopology.MakeTopologyResInfo(hugepages2Mi, "384Mi", "384Mi"),
+						noderesourcetopology.MakeTopologyResInfo(nicResourceName, "16", "16")}).
+				Zone(
+					topologyv1alpha1.ResourceInfoList{
+						noderesourcetopology.MakeTopologyResInfo(cpu, "32", "32"),
+						noderesourcetopology.MakeTopologyResInfo(memory, "64Gi", "64Gi"),
+						noderesourcetopology.MakeTopologyResInfo(hugepages2Mi, "512Mi", "512Mi"),
+						noderesourcetopology.MakeTopologyResInfo(nicResourceName, "32", "32"),
+					}).Obj(),
+			// we set all available resource as 0
+			// because we want all these tests to be running against fake-node-1
+			MakeNRT().Name("fake-node-2").Policy(topologyv1alpha1.SingleNUMANodeContainerLevel).
+				Zone(
+					topologyv1alpha1.ResourceInfoList{
+						noderesourcetopology.MakeTopologyResInfo(cpu, "32", "0"),
+						noderesourcetopology.MakeTopologyResInfo(memory, "64Gi", "0"),
+						noderesourcetopology.MakeTopologyResInfo(hugepages2Mi, "384Mi", "0"),
+						noderesourcetopology.MakeTopologyResInfo(nicResourceName, "16", "0"),
+					}).
+				Zone(
+					topologyv1alpha1.ResourceInfoList{
+						noderesourcetopology.MakeTopologyResInfo(cpu, "32", "0"),
+						noderesourcetopology.MakeTopologyResInfo(memory, "64Gi", "0"),
+						noderesourcetopology.MakeTopologyResInfo(hugepages2Mi, "512Mi", "0"),
+						noderesourcetopology.MakeTopologyResInfo(nicResourceName, "32", "0"),
+					}).Obj(),
+		}
+		expectedNodes := []string{"fake-node-1"}
+		// if there's an error we expect the pod
+		// to not be found on any node
+		if len(e.errMsg) > 0 {
+			expectedNodes = []string{}
+		}
+		te := nrtTestEntry{
+			name:                   e.description,
+			pods:                   []*v1.Pod{p.Obj()},
+			nodeResourceTopologies: nodeTopologies,
+			expectedNodes:          expectedNodes,
+			errMsg:                 e.errMsg,
+		}
+		teList = append(teList, te)
+	}
+	return teList
+}
+
+func podFailedScheduling(c clientset.Interface, podNamespace, podName string) ([]v1.Event, error) {
+	var failedSchedulingEvents []v1.Event
+	opt := metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("involvedObject.name=%s", podName),
+		TypeMeta:      metav1.TypeMeta{Kind: "Pod"},
+	}
+	events, err := c.CoreV1().Events(podNamespace).List(context.TODO(), opt)
+	if err != nil {
+		return failedSchedulingEvents, err
 	}
 
-	for i, resInfoList := range resInfoLists {
-		z := topologyv1alpha1.Zone{
-			Name:      fmt.Sprintf("node-%d", i),
-			Type:      "Node",
-			Resources: resInfoList,
+	for _, e := range events.Items {
+		if e.Reason == "FailedScheduling" {
+			failedSchedulingEvents = append(failedSchedulingEvents, e)
 		}
-		nrt.Zones = append(nrt.Zones, z)
 	}
-	return nrt
+	return failedSchedulingEvents, nil
 }
