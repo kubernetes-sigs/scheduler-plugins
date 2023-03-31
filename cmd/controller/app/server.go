0383bfded2e399aca092ab45a735961e5414225f
czybjtu <smartczy@outlook.com>
2023-03-31 18:19:25 +0800

    Delete Useless options in controller

--- cmd/controller/app/server.go
+++ cmd/controller/app/server.go
@@ -17,22 +17,9 @@ limitations under the License.
 package app
 
 import (
-	"context"
-	"os"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/uuid"
-	"k8s.io/apiserver/pkg/server"
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	restclient "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/leaderelection"
-	"k8s.io/client-go/tools/leaderelection/resourcelock"
-	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -40,8 +27,6 @@ import (
 
 	schedulingv1a1 "sigs.k8s.io/scheduler-plugins/apis/scheduling/v1alpha1"
 	"sigs.k8s.io/scheduler-plugins/pkg/controllers"
-	schedclientset "sigs.k8s.io/scheduler-plugins/pkg/generated/clientset/versioned"
-	schedformers "sigs.k8s.io/scheduler-plugins/pkg/generated/informers/externalversions"
 )
 
 var (
@@ -55,38 +40,10 @@ func init() {
 	utilruntime.Must(schedulingv1a1.AddToScheme(scheme))
 }
 
-func newConfig(kubeconfig, master string, inCluster bool) (*restclient.Config, error) {
-	var (
-		config *rest.Config
-		err    error
-	)
-	if inCluster {
-		config, err = rest.InClusterConfig()
-	} else {
-		config, err = clientcmd.BuildConfigFromFlags(master, kubeconfig)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return config, nil
-}
-
 func Run(s *ServerRunOptions) error {
-	ctx := context.Background()
-	config, err := newConfig(s.KubeConfig, s.MasterUrl, s.InCluster)
-	if err != nil {
-		klog.ErrorS(err, "Failed to parse config")
-		os.Exit(1)
-	}
+	config := ctrl.GetConfigOrDie()
 	config.QPS = float32(s.ApiServerQPS)
 	config.Burst = s.ApiServerBurst
-	stopCh := server.SetupSignalHandler()
-	schedClient := schedclientset.NewForConfigOrDie(config)
-	kubeClient := kubernetes.NewForConfigOrDie(config)
-
-	schedInformerFactory := schedformers.NewSharedInformerFactory(schedClient, 0)
-
-	coreInformerFactory := informers.NewSharedInformerFactory(kubeClient, 0)
 
 	// Controller Runtime Controllers
 	ctrl.SetLogger(klogr.New())
@@ -105,16 +62,18 @@ func Run(s *ServerRunOptions) error {
 	}
 
 	if err = (&controllers.PodGroupReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:  mgr.GetClient(),
+		Scheme:  mgr.GetScheme(),
+		Workers: s.Workers,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "PodGroup")
 		return err
 	}
 
 	if err = (&controllers.ElasticQuotaReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:  mgr.GetClient(),
+		Scheme:  mgr.GetScheme(),
+		Workers: s.Workers,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "ElasticQuota")
 		return err
@@ -129,53 +88,9 @@ func Run(s *ServerRunOptions) error {
 		return err
 	}
 
-	run := func(ctx context.Context) {
-		setupLog.Info("starting manager")
-		if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-			setupLog.Error(err, "unable to start manager")
-			panic(err)
-		}
-
-		select {}
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "unable to start manager")
+		return err
 	}
-	schedInformerFactory.Start(stopCh)
-	coreInformerFactory.Start(stopCh)
-	if !s.EnableLeaderElection {
-		run(ctx)
-	} else {
-		id, err := os.Hostname()
-		if err != nil {
-			return err
-		}
-		// add a uniquifier so that two processes on the same host don't accidentally both become active
-		id = id + "_" + string(uuid.NewUUID())
-
-		rl, err := resourcelock.New("endpoints",
-			"kube-system",
-			"sched-plugins-controller",
-			kubeClient.CoreV1(),
-			kubeClient.CoordinationV1(),
-			resourcelock.ResourceLockConfig{
-				Identity: id,
-			})
-		if err != nil {
-			klog.ErrorS(err, "Resource lock creation failed")
-			os.Exit(1)
-		}
-
-		leaderelection.RunOrDie(context.TODO(), leaderelection.LeaderElectionConfig{
-			Lock: rl,
-			Callbacks: leaderelection.LeaderCallbacks{
-				OnStartedLeading: run,
-				OnStoppedLeading: func() {
-					klog.ErrorS(err, "Leaderelection lost")
-					os.Exit(1)
-				},
-			},
-			Name: "scheduler-plugins controller",
-		})
-	}
-
-	<-stopCh
 	return nil
 }
