dca7acffc276fcf1770663d03528190fe66c755d
Wei Huang <weihuang@apple.com>
2023-11-17 16:49:00 -0800

    work around k/k#121630

--- test/integration/loadVariationRiskBalancing_test.go
+++ test/integration/loadVariationRiskBalancing_test.go
@@ -113,6 +113,10 @@ func TestLoadVariationRiskBalancingPlugin(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	// Work around https://github.com/kubernetes/kubernetes/issues/121630.
+	cfg.Profiles[0].Plugins.PreScore = schedapi.PluginSet{
+		Disabled: []schedapi.Plugin{{Name: "*"}},
+	}
 	cfg.Profiles[0].Plugins.Score = schedapi.PluginSet{
 		Enabled: []schedapi.Plugin{
 			{Name: loadvariationriskbalancing.Name},
