24216c64c37b5ce38886107e85e954ed6b12cab7
Wei Zhang <kweizh@gmail.com>
2024-01-31 00:33:36 +0800

    vendor: vendor k8s v1.27.10
    Signed-off-by: Wei Zhang <kweizh@gmail.com>

--- vendor/golang.org/x/tools/go/analysis/internal/checker/checker.go
+++ vendor/golang.org/x/tools/go/analysis/internal/checker/checker.go
@@ -17,7 +17,6 @@ import (
 	"go/format"
 	"go/token"
 	"go/types"
-	"io/ioutil"
 	"log"
 	"os"
 	"reflect"
@@ -343,20 +342,28 @@ func applyFixes(roots []*action) error {
 				for _, edit := range sf.TextEdits {
 					// Validate the edit.
 					// Any error here indicates a bug in the analyzer.
-					file := act.pkg.Fset.File(edit.Pos)
+					start, end := edit.Pos, edit.End
+					file := act.pkg.Fset.File(start)
 					if file == nil {
 						return fmt.Errorf("analysis %q suggests invalid fix: missing file info for pos (%v)",
-							act.a.Name, edit.Pos)
+							act.a.Name, start)
 					}
-					if edit.Pos > edit.End {
+					if !end.IsValid() {
+						end = start
+					}
+					if start > end {
 						return fmt.Errorf("analysis %q suggests invalid fix: pos (%v) > end (%v)",
-							act.a.Name, edit.Pos, edit.End)
+							act.a.Name, start, end)
 					}
-					if eof := token.Pos(file.Base() + file.Size()); edit.End > eof {
+					if eof := token.Pos(file.Base() + file.Size()); end > eof {
 						return fmt.Errorf("analysis %q suggests invalid fix: end (%v) past end of file (%v)",
-							act.a.Name, edit.End, eof)
+							act.a.Name, end, eof)
 					}
-					edit := diff.Edit{Start: file.Offset(edit.Pos), End: file.Offset(edit.End), New: string(edit.NewText)}
+					edit := diff.Edit{
+						Start: file.Offset(start),
+						End:   file.Offset(end),
+						New:   string(edit.NewText),
+					}
 					editsForTokenFile[file] = append(editsForTokenFile[file], edit)
 				}
 			}
@@ -425,7 +432,7 @@ func applyFixes(roots []*action) error {
 
 	// Now we've got a set of valid edits for each file. Apply them.
 	for path, edits := range editsByPath {
-		contents, err := ioutil.ReadFile(path)
+		contents, err := os.ReadFile(path)
 		if err != nil {
 			return err
 		}
@@ -440,7 +447,7 @@ func applyFixes(roots []*action) error {
 			out = formatted
 		}
 
-		if err := ioutil.WriteFile(path, out, 0644); err != nil {
+		if err := os.WriteFile(path, out, 0644); err != nil {
 			return err
 		}
 	}
@@ -480,17 +487,17 @@ func validateEdits(edits []diff.Edit) ([]diff.Edit, int) {
 // diff3Conflict returns an error describing two conflicting sets of
 // edits on a file at path.
 func diff3Conflict(path string, xlabel, ylabel string, xedits, yedits []diff.Edit) error {
-	contents, err := ioutil.ReadFile(path)
+	contents, err := os.ReadFile(path)
 	if err != nil {
 		return err
 	}
 	oldlabel, old := "base", string(contents)
 
-	xdiff, err := diff.ToUnified(oldlabel, xlabel, old, xedits)
+	xdiff, err := diff.ToUnified(oldlabel, xlabel, old, xedits, diff.DefaultContextLines)
 	if err != nil {
 		return err
 	}
-	ydiff, err := diff.ToUnified(oldlabel, ylabel, old, yedits)
+	ydiff, err := diff.ToUnified(oldlabel, ylabel, old, yedits, diff.DefaultContextLines)
 	if err != nil {
 		return err
 	}
