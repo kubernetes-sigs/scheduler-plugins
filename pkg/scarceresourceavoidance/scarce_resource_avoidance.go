02c34fcbbaa30ea00120148d9fe5ead5dc867e37
LY-today <724102053@qq.com>
2024-12-23 14:14:25 +0800

    feat: add NodeResourcesFitPlus and ScarceResourceAvoidance plugin
    Signed-off-by: LY-today <724102053@qq.com>

---
+++ pkg/scarceresourceavoidance/scarce_resource_avoidance.go
@@ -0,0 +1,158 @@
+package scarceresourceavoidance
+
+import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/pkg/api/v1/resource"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+	"sigs.k8s.io/scheduler-plugins/apis/config"
+)
+
+const (
+	// Name is plugin name
+	Name = "ScarceResourceAvoidance"
+)
+
+var (
+	_ framework.ScorePlugin = &Plugin{}
+)
+
+type Plugin struct {
+	handle framework.Handle
+	args   *config.ScarceResourceAvoidanceArgs
+}
+
+func New(_ context.Context, args runtime.Object, handle framework.Handle) (framework.Plugin, error) {
+
+	sampleArgs2, ok := args.(*config.ScarceResourceAvoidanceArgs)
+
+	if !ok {
+		return nil, fmt.Errorf("want args to be of type ResourceTypesArgs, got %T", args)
+	}
+
+	return &Plugin{
+		handle: handle,
+		args:   sampleArgs2,
+	}, nil
+}
+
+func (s *Plugin) Name() string {
+	return Name
+}
+
+func (s *Plugin) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
+	nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
+	if err != nil {
+		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("getting node %q from Snapshot: %v", nodeName, err))
+	}
+
+	podRequest := computePodResourceRequest(p)
+	podRequestResource, nodeAllocatableResource := fitsRequest(podRequest.Resource, nodeInfo)
+	diffNames := difference(nodeAllocatableResource, podRequestResource)
+	intersectNames := intersection(diffNames, s.args.Resources)
+
+	if len(diffNames) == 0 || len(intersectNames) == 0 {
+		return framework.MaxNodeScore, nil
+	}
+	scores := resourceTypesScore(int64(len(intersectNames)), int64(len(diffNames)))
+
+	return scores, nil
+}
+
+func intersection(slice1, slice2 []v1.ResourceName) []v1.ResourceName {
+	m := make(map[v1.ResourceName]struct{})
+	var result []v1.ResourceName
+
+	for _, v := range slice2 {
+		m[v] = struct{}{}
+	}
+
+	for _, v := range slice1 {
+		if _, found := m[v]; found {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func difference(slice1, slice2 []v1.ResourceName) []v1.ResourceName {
+	var result []v1.ResourceName
+	m := make(map[v1.ResourceName]struct{})
+	for _, v := range slice2 {
+		m[v] = struct{}{}
+	}
+
+	for _, v := range slice1 {
+		if _, found := m[v]; !found {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func (p *Plugin) ScoreExtensions() framework.ScoreExtensions {
+	return nil
+}
+
+type preFilterState struct {
+	framework.Resource
+}
+
+func computePodResourceRequest(pod *v1.Pod) *preFilterState {
+	// pod hasn't scheduled yet so we don't need to worry about InPlacePodVerticalScalingEnabled
+	reqs := resource.PodRequests(pod, resource.PodResourcesOptions{})
+	result := &preFilterState{}
+	result.SetMaxResource(reqs)
+	return result
+}
+
+func fitsRequest(podRequest framework.Resource, nodeInfo *framework.NodeInfo) ([]v1.ResourceName, []v1.ResourceName) {
+	var podRequestResource []v1.ResourceName
+	var nodeRequestResource []v1.ResourceName
+
+	if podRequest.MilliCPU > 0 {
+		podRequestResource = append(podRequestResource, v1.ResourceCPU)
+	}
+
+	if nodeInfo.Allocatable.MilliCPU > 0 {
+		nodeRequestResource = append(nodeRequestResource, v1.ResourceCPU)
+	}
+
+	if podRequest.Memory > 0 {
+		podRequestResource = append(podRequestResource, v1.ResourceMemory)
+	}
+
+	if nodeInfo.Allocatable.Memory > 0 {
+		nodeRequestResource = append(nodeRequestResource, v1.ResourceMemory)
+	}
+
+	if podRequest.EphemeralStorage > 0 {
+		podRequestResource = append(podRequestResource, v1.ResourceEphemeralStorage)
+	}
+
+	if nodeInfo.Allocatable.EphemeralStorage > 0 {
+		nodeRequestResource = append(nodeRequestResource, v1.ResourceEphemeralStorage)
+	}
+
+	for rName, rQuant := range podRequest.ScalarResources {
+		if rQuant > 0 {
+			podRequestResource = append(podRequestResource, rName)
+		}
+	}
+
+	for rName, rQuant := range nodeInfo.Allocatable.ScalarResources {
+		if rQuant > 0 {
+			nodeRequestResource = append(nodeRequestResource, rName)
+		}
+	}
+
+	return podRequestResource, nodeRequestResource
+}
+func resourceTypesScore(requestsSourcesNum, allocatablesSourcesNum int64) int64 {
+	return (allocatablesSourcesNum - requestsSourcesNum) * framework.MaxNodeScore / allocatablesSourcesNum
+}
