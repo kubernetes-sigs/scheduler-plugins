2c2350b9dc91c1dfd36a6025a230a5c38297d108
henrikdchristensen <djl587@alumni.ku.dk>
2025-09-21 07:37:15 +0200

    cleanup

--- pkg/mycrossnodepreemption/strategy_helpers.go
+++ pkg/mycrossnodepreemption/strategy_helpers.go
@@ -9,14 +9,15 @@ import (
 	"k8s.io/klog/v2"
 )
 
-// TODO: Reach to here in this file...
-
 // OptimizationCadence indicates how we optimize (for every pod, batch, continuous).
 type OptimizationCadence int
 
 const (
+	// OptimizeEvery indicates we optimize for every new pod.
 	OptimizeEvery OptimizationCadence = iota
+	// OptimizeBatch indicates we optimize in batches.
 	OptimizeBatch
+	// OptimizeContinuous indicates we optimize continuously.
 	OptimizeContinuous
 )
 
@@ -24,7 +25,9 @@ const (
 type OptimizationAt int
 
 const (
+	// OptimizeAtPreEnqueue indicates we optimize at the PreEnqueue phase.
 	OptimizeAtPreEnqueue OptimizationAt = iota
+	// OptimizeAtPostFilter indicates we optimize at the PostFilter phase.
 	OptimizeAtPostFilter
 )
 
@@ -32,9 +35,13 @@ const (
 type StrategyDecision int
 
 const (
+	// DecidePassThrough indicates we let the pod pass through without optimization.
 	DecidePassThrough StrategyDecision = iota
+	// DecideBatch indicates we batch the pod for later optimization.
 	DecideBatch
+	// DecideEvery indicates we should optimize this new single pod.
 	DecideEvery
+	// DecideBlockActive indicates we block the pod due to an active plan.
 	DecideBlockActive
 )
 
@@ -42,8 +49,11 @@ const (
 type Phase int
 
 const (
+	// PhaseNone indicates we are not in a phase we care about.
 	PhaseNone Phase = iota // for periodic optimization
+	// PhasePreEnqueue indicates we are in the PreEnqueue phase.
 	PhasePreEnqueue
+	// PhasePostFilter indicates we are in the PostFilter phase.
 	PhasePostFilter
 )
 
