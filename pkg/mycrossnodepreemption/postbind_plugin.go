0aab8dae3503b6d454756a6ab261568d9e865d02
henrikdchristensen <djl587@alumni.ku.dk>
2025-09-22 19:51:34 +0200

    rename

--- pkg/mycrossnodepreemption/postbind_plugin.go
+++ pkg/mycrossnodepreemption/postbind_plugin.go
@@ -13,36 +13,36 @@ import (
 // PostBind is called after a pod is bound to a node.
 // It is used to check if the active scheduling plan is still in progress.
 // postbind_plugin.go
-func (pl *MyCrossNodePreemption) PostBind(ctx context.Context, _ *framework.CycleState, pod *v1.Pod, _ string) {
+func (pl *MyCrossNodePreemption) PostBind(ctx context.Context, _ *framework.CycleState, pending *v1.Pod, _ string) {
 
 	stage := "PostBind"
 
 	// Early exit if system pods or if no active plan
 	ap := pl.getActivePlan()
-	if pod.Namespace == SystemNamespace || ap == nil {
+	if pending.Namespace == SystemNamespace || ap == nil {
 		return
 	}
 
 	// Check if the pod is relevant to the active plan
 	relevant := false
-	if _, ok := ap.PlacementByName[combineNsName(pod.Namespace, pod.Name)]; ok {
+	if _, ok := ap.PlacementByName[combineNsName(pending.Namespace, pending.Name)]; ok {
 		relevant = true
-	} else if wk, ok := topWorkload(pod); ok {
+	} else if wk, ok := topWorkload(pending); ok {
 		_, relevant = ap.WorkloadPerNodeCnts[wk.String()]
 	}
 	if !relevant {
-		klog.V(MyV).InfoS(msg(stage, "irrelevant"), "pod", klog.KObj(pod))
+		klog.V(MyV).InfoS(msg(stage, "irrelevant"), "pod", klog.KObj(pending))
 		return
 	}
 
 	// Check if the plan is completed
-	ok, err := pl.isPlanCompleted(ctx, ap, pod)
+	ok, err := pl.isPlanCompleted(ctx, ap, pending)
 	if err != nil {
 		klog.V(MyV).ErrorS(err, msg(stage, InfoPlanCompletionFailed))
 		return
 	}
 	if !ok {
-		klog.V(MyV).InfoS(msg(stage, InfoActivePlanInProgress), "planID", ap.ID, "pod", klog.KObj(pod))
+		klog.V(MyV).InfoS(msg(stage, InfoActivePlanInProgress), "planID", ap.ID, "pod", klog.KObj(pending))
 		return
 	}
 	// Complete the plan
