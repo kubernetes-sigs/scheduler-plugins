f86b3115c2b3735db8c0cf064eeb5e00e466e76a
Shereen Haj <shajmakh@redhat.com>
2026-06-09 15:48:32 +0300

    nrt: overreserve: consider only sidecars containers
    NRT is using PodResourcesAPI to determine the PFP sign which is compared
    with the one computed by scheduler and is expected to both be synced,
    hence computing both from each side need to use the same filter on pods.
    
    Non-guaranteed pods with regular (non-restartable) init containers that request
    exclusive non-native resources (i.e. devices) are not included in the
    NRT-PFP sign because they are not reported in by the PodResourcesAPI
    (see https://github.com/kubernetes/kubernetes/issues/126765). The
    reason for that is that regular init containers are supposed to run
    fast. The mismatch in PFPs causes the scheduler to stall on `invalid
    topology data` for the node on which this opd is running.
    
    `AreExclusiveForPod` was so far used in two cases: cache invalidation on foreign
    pod detection; and on update the cache PFP on resync. In both we want to
    guarantee that the scheduler address the correct pods, those same ones
    that are used for cache invalidation and in PFP build up.
    
    This commit matches the pods filter used by NRT to compute PFP and
    excludes non-restartable init-containers from the scheduler computed PFP.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- pkg/noderesourcetopology/resourcerequests/exclusive_test.go
+++ pkg/noderesourcetopology/resourcerequests/exclusive_test.go
@@ -22,13 +22,15 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 )
 
 type testCase struct {
-	name              string
-	pod               *corev1.Pod
-	expectedNonNative bool
-	expectedExclusive bool
+	name                    string
+	pod                     *corev1.Pod
+	expectedNonNative       bool
+	expectedExclusive       bool
+	expectedSteadyExclusive bool
 }
 
 func TestIncludeNonNative(t *testing.T) {
@@ -43,13 +45,13 @@ func TestIncludeNonNative(t *testing.T) {
 	}
 }
 
-func TestAreExclusiveForPod(t *testing.T) {
+func TestAreExclusiveForSteadyState(t *testing.T) {
 	tcases := coreTestCases()
 	for _, tt := range tcases {
 		t.Run(tt.name, func(t *testing.T) {
-			got := AreExclusiveForPod(tt.pod)
-			if got != tt.expectedExclusive {
-				t.Errorf("%s: exclusive resources detected %v expected %v", tt.name, got, tt.expectedExclusive)
+			got := AreExclusiveForSteadyState(tt.pod)
+			if got != tt.expectedSteadyExclusive {
+				t.Errorf("%s: steady exclusive resources detected %v expected %v", tt.name, got, tt.expectedSteadyExclusive)
 			}
 		})
 	}
@@ -65,8 +67,9 @@ func coreTestCases() []testCase {
 					Namespace: "default",
 				},
 			},
-			expectedNonNative: false,
-			expectedExclusive: false,
+			expectedNonNative:       false,
+			expectedExclusive:       false,
+			expectedSteadyExclusive: false,
 		},
 		{
 			name: "single-container-gu-no-devs",
@@ -93,8 +96,9 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: false,
-			expectedExclusive: true,
+			expectedNonNative:       false,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-initcontainer-gu-no-devs",
@@ -121,8 +125,39 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: false,
-			expectedExclusive: true,
+			expectedNonNative:       false,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: false,
+		},
+		{
+			name: "single-sidecar-initcontainer-gu-no-devs",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "pod",
+					Namespace: "default",
+				},
+				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{
+						{
+							Name: "cnt",
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									corev1.ResourceCPU:    resource.MustParse("4"),
+									corev1.ResourceMemory: resource.MustParse("2Gi"),
+								},
+								Requests: corev1.ResourceList{
+									corev1.ResourceCPU:    resource.MustParse("4"),
+									corev1.ResourceMemory: resource.MustParse("2Gi"),
+								},
+							},
+							RestartPolicy: ptr.To(corev1.ContainerRestartPolicyAlways),
+						},
+					},
+				},
+			},
+			expectedNonNative:       false,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-container-devs-only",
@@ -147,8 +182,9 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: true,
-			expectedExclusive: true,
+			expectedNonNative:       true,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-initcontainer-devs-only",
@@ -173,8 +209,37 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: true,
-			expectedExclusive: true,
+			expectedNonNative:       true,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: false,
+		},
+		{
+			name: "single-sidecar-initcontainer-devs-only",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "pod",
+					Namespace: "default",
+				},
+				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{
+						{
+							Name: "cnt",
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									corev1.ResourceName("veryfast.io/fpga"): resource.MustParse("1"),
+								},
+								Requests: corev1.ResourceList{
+									corev1.ResourceName("veryfast.io/fpga"): resource.MustParse("1"),
+								},
+							},
+							RestartPolicy: ptr.To(corev1.ContainerRestartPolicyAlways),
+						},
+					},
+				},
+			},
+			expectedNonNative:       true,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-container-gu-core-and-devs",
@@ -203,8 +268,9 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: true,
-			expectedExclusive: true,
+			expectedNonNative:       true,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-container-nongu-cpus-and-devs",
@@ -231,8 +297,9 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: true,
-			expectedExclusive: true,
+			expectedNonNative:       true,
+			expectedExclusive:       true,
+			expectedSteadyExclusive: true,
 		},
 		{
 			name: "single-container-nongu-cpus-only",
@@ -257,8 +324,9 @@ func coreTestCases() []testCase {
 					},
 				},
 			},
-			expectedNonNative: false,
-			expectedExclusive: false,
+			expectedNonNative:       false,
+			expectedExclusive:       false,
+			expectedSteadyExclusive: false,
 		},
 	}
 }
